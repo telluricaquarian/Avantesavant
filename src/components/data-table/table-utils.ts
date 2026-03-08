aee515d8208ab30a616b1380f2cf85c3b2c52ddb
Llewellyn Y. Fisher <llewellyny.fisher@Llewellyns-MacBook-Air.local>
2026-03-07 16:14:23 -0800

    Initialize Avantesavant from dashboard template

---
+++ src/components/data-table/table-utils.ts
@@ -0,0 +1,9 @@
+"use no memo";
+
+import type { ColumnDef } from "@tanstack/react-table";
+
+import { dragColumn } from "./drag-column";
+
+export function withDndColumn<T>(columns: ColumnDef<T>[]): ColumnDef<T>[] {
+  return [dragColumn as ColumnDef<T>, ...columns];
+}
